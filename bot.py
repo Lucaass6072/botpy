e15c7ef82f756c4cd013bf3e07db62990efe5990
Lucaass6072 <101116879+Lucaass6072@users.noreply.github.com>
2022-04-02 15:50:11 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,54 +1,46 @@
-# welcome-bot
-## this bot will send a welcome message and assign a role
-
-# import all necessary commands and libraries
 import discord
-import asyncio
-intents = discord.Intents(members=True)
-client=discord.Client(intents=intents)
-welcomechannel = await client.fetch_channel(channel_id) # https://support.discord.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID
-# Make sure you get the ID of your channel by right-clicking it and clicking `Copy ID`. Make sure developer mode is on!
+from discord.ext import commands
+import random
+
+client = commands.Bot(command_prefix = ">", case_insensitive = True)
+
 @client.event
 async def on_ready():
-    print('logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('-----')
- # Customise the message below to what you want to send new users!
-newUserMessage = """
-You
-can
-put
-your
-multiline
-message
-here!
-"""
+  print('Entramos como {0.user}' .format(client))
 
-@client.event
-async def on_member_join(member):
-    print("Recognised that a member called " + member.name + " joined")
-    try: 
-        await client.send_message(member, newUserMessage)
-        print("Sent message to " + member.name)
-    except:
-        print("Couldn't message " + member.name)
-    embed=discord.Embed(
-        title="Welcome "+member.name+"!"
-        description="We're so glad you're here!"
-        color=discord.Color.green()
-    )
-        
-    role = discord.utils.get(member.server.roles, name="name-of-your-role") #  Gets the member role as a `role` object
-    await client.add_roles(member, role) # Gives the role to the user
-    print("Added role '" + role.name + "' to " + member.name)
+@client.command()
+async def ola(ctx):
+  await ctx.send(f'Olá, {ctx.author}')
 
-@client.event
-async def on_member_leave(member):
-    print("Recognised that a member called " + member.name + " left")
-    embed=discord.Embed(
-        title="😢 Goodbye "+member.name+"!",
-        description="Until we meet again old friend." # A description isn't necessary, you can delete this line if you don't want a description.
-        color=discord.Color.red() # There are lots of colors, you can check them here: https://discordpy.readthedocs.io/en/latest/api.html?highlight=discord%20color#discord.Colour
-    )
+@client.command()
+async def dado(ctx, numero):
+  variavel = random.randint(1,int(numero))
+  await ctx.send(f'O número que saiu no dado é {variavel}')
+
+@client.command()
+async def enviarembed(ctx):
+  embed = discord.Embed(
+    title = 'Título',
+    description = 'Descrição',
+    colour = 11598249
+  )
+
+  embed.set_author(name='Autor', icon_url='https://www.tibiawiki.com.br/images/e/e2/Adamant_Shield.gif')
+
+  embed.set_thumbnail(url='https://www.tibiawiki.com.br/images/b/bb/Ancient_Tiara.gif')
+
+  embed.set_image(url='https://image.api.playstation.com/vulcan/img/rnd/202010/0911/bZBvuQ98T04JvJCR7lJFYssZ.jpg')
+
+  embed.set_footer(text='Esse é o footer')
+
+  embed.add_field(name='Nome do Field', value='Valor do Field', inline=False)
+  embed.add_field(name='Nome do Field', value='Valor do Field', inline=True)
+  embed.add_field(name='Nome do Field', value='Valor do Field', inline=True)
+
+  await ctx.send(embed = embed)
+
+
+
+
+
 client.run('token') 
